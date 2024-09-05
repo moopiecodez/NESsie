5d46f4383c3b5f60ece92a9ce7e3fb350cd75165
Maurane van der Stoep <mauranevds@gmail.com>
2024-09-05 16:44:59 +0100

    update instruction header file

--- instructions.h
+++ instructions.h
@@ -1,57 +1,21 @@
-#include <stdint.h>
 #define BYTE uint8_t
 
-void ADC(BYTE memory);
+typedef void Instruction(CPU *);
+Instruction BRK;
+//Read instructions:
+Instruction LDA;
+Instruction LDX;
+Instruction LDY;
+Instruction AND;
 
-void BRK();
-void RTI();
-void ORA(BYTE compared);
-//may need to change to take memory address to update
-void ASL(BYTE *byte);
-void PHP(); 
-void PHA();
-void PLP();
-void PLA();
 
-//helper functions
-void push_to_stack(BYTE reg);
-BYTE pull_from_stack();
-void compare_flags(BYTE reg, BYTE memory);
+//RMW instructions:
+Instruction ASL;
+Instruction LSR;
 
-//clear flag instructions
-void CLC();
-void CLD();
-void CLI();
-void CLV();
+// Write instructions:
+Instruction STX;
+Instruction STA;
+Instruction STY;
 
-//comparison instructions
-void CMP(BYTE memory);
-void CPX(BYTE memory);
-void CPY(BYTE memory);
-
-void AND(BYTE memory);
-
-//branch instructions
-void BCC(BYTE rdisplacement);
-void BCS(BYTE rdisplacement);
-void BEQ(BYTE rdisplacement);
-void BMI(BYTE rdisplacement);
-void BNE(BYTE rdisplacement);
-void BPL(BYTE rdisplacement);
-void BVC(BYTE rdisplacement);
-void BVS(BYTE rdisplacement);
-
-void BIT(BYTE memory);
-
-//decrement instructions
-void DEC(BYTE *memory);
-void DEX();
-void DEY();
-
-void EOR(BYTE memory);
-
-//increment instructions
-void INC(BYTE *memory);
-void INX();
-void INY();
-
+Instruction CLC;
