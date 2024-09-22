c307f04fc16ef6c2b300ac0129b963d365481797
Maurane van der Stoep <mauranevds@gmail.com>
2024-09-22 14:14:26 +0100

    change to polling sdl events in game loop

--- sdl/experiment.c
+++ sdl/experiment.c
@@ -1,31 +1,36 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 
+#define NTSC_SCANLINES_TOTAL 262
+#define NTSC_SCANLINES_RENDERED 240
+#define NTSC_SCANLINE_WIDTH_TOTAL 341
+#define NTSC_SCANLINE_WIDTH_RENDERED 256
+#define MODERN_WIDTH 1280
+#define MODERN_HEIGHT 960
+
 SDL_Window *create_screen();
+void draw();
+void handle_events(bool *);
 
 int main(void) {
     Uint32 flags;
     bool quit;
-    SDL_Event event;
 
     flags = SDL_INIT_VIDEO;
-
     SDL_Init(flags);
-    SDL_Window *Nessie_screen = create_screen();
+    // SDL_Window *Nessie_screen = create_screen();
+    create_screen();
 
     quit = false;
+    int clock = 0;
     while (!quit) {
-        SDL_WaitEvent(&event);
-
-        switch (event.type) {
-            case SDL_QUIT:
-                quit = true;
-                break;
-        }
+        printf("Clock cycle: %d\n", clock);
+        handle_events(&quit);
+        draw();
+        clock++;
     }
 
     SDL_Quit();
-
     return 0;
 }
 
@@ -34,8 +39,8 @@ SDL_Window *create_screen() {
     char *title = "Opening to Nessie's soul";
     int screen_pos_x = SDL_WINDOWPOS_CENTERED;
     int screen_pos_y = SDL_WINDOWPOS_CENTERED;
-    int screen_width = 1280;
-    int screen_height = 960;
+    int screen_width = NTSC_SCANLINE_WIDTH_TOTAL;
+    int screen_height = NTSC_SCANLINES_TOTAL;
     Uint32 screen_flags = 0;
 
     screen = SDL_CreateWindow(
@@ -43,3 +48,26 @@ SDL_Window *create_screen() {
         screen_width, screen_height, screen_flags);
     return screen;
 }
+
+void draw() {
+    printf("I drawd\n");
+}
+
+void handle_events(bool *quit) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+        case SDL_QUIT:
+            *quit = true;
+        }
+    }
+}
+
+//forloop can't be in this while loop directly as prevents exit
+    // int PPU_count;  
+    // for(int i = 0; i < NTSC_SCANLINES_TOTAL; i++) {
+    //     for(int j = 0; j < NTSC_SCANLINE_WIDTH_TOTAL; j++){
+    //         PPU_count++;
+    //         printf("Scanline: %d, Pixel: %d, PPU cycles: %d \n", i, j, PPU_count);
+    //     }
+    // }
