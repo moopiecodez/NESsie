93d7403b1754ee53eb0e955a7794c601780c684f
Maurane van der Stoep <mauranevds@gmail.com>
2024-09-26 14:46:42 +0100

    display character on screen

--- sdl/experiment.c
+++ sdl/experiment.c
@@ -4,9 +4,35 @@
 
 void handle_events(bool *);
 
+uint8_t dummypat[0x10] = {  0x18, 0x38, 0x18, 0x18, 0x18, 0x18, 0x7E, 0x00, 
+                            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
 int main(void) {
     Uint32 init_flags = SDL_INIT_VIDEO;
     SDL_Init(init_flags);
+
+    int i;
+
+    uint8_t plane1[8];
+    uint8_t plane2[8];
+    uint8_t result[8];
+    for(i = 0; i < 8; i++) {
+        plane1[i] = dummypat[i];
+        plane2[i] = dummypat[i+8];
+    }
+ 
+    for(i = 0; i < 8; i ++) {
+            result[i] = plane1[i] + plane2[i];
+            printf("p1: %d, p2: %d, result: %d\n",plane1[i], plane2[i], result[i]);
+        }
+/*   
+    for(i = 0; i < 8; i++) {
+        printf("\n");
+        for(int j = 7; j >= 0; j--) {
+            printf("%d", plane1[i] >> j & ~(~0<<1));
+            // screen_pixel(screen, 0, 0, 255, j, i);
+        }
+    } */
 
     Screen screen = create_screen();
 
@@ -16,10 +42,20 @@ int main(void) {
     while (!quit) {
         handle_events(&quit);
         //update game state
-        for(int i = 0; i < NTSC_SCANLINES_RENDERED; i++) {
-            screen_pixel(screen, 0, 0, 255, i, i);
+        for(i = 0; i < 8; i++) {
+            printf("\n");
+            for(int j = 7; j >= 0; j--) {
+                uint8_t pixel_on = result[i] >> j & ~(~0<<1);
+                printf("%d", pixel_on);
+                if(pixel_on != 0){
+                    screen_pixel(screen, 0, 0, 255, j, i+10);
+                }
+            }
         }
-        //draw current frame
+        // for(int i = 0; i < NTSC_SCANLINES_RENDERED; i++) {
+        //     screen_pixel(screen, 0, 0, 255, i, i);
+        // }
+        // draw current frame
         screen_draw_frame(screen);
         clock++;
     }
